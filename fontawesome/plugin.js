f0e1d52aa944e8dd69ce3ef4a031ed342e24c0d8
Đặng Đình Tứ <webdep24@gmail.com>
2016-01-11 17:47:54 +0700

    update author and code

--- fontawesome/plugin.js
+++ fontawesome/plugin.js
@@ -1,6 +1,35 @@
 /*
-	Author	: Michael Janea (https://facebook.com/mbjanea)
-	Version	: 1.2
+	Author	: Dang Dinh Tu (https://dangdinhtu.com)
+	Version	: 1.1
 */
 
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('7.j.e(\'b\',{z:\'A\',x:\'b\',l:a(k){k.C.e(\'N\',{p:\'q t u\',w:\'<c 4="" 8=""></c>\',f:\'i\',D:\'c(!g){8}\',O:a(1){P 1.r==\'c\'&&1.s(\'g\')},l:a(){0.d(\'4\',0.1.v(\'4\'));0.d(\'6\',0.1.h(\'6\'));0.d(\'5\',0.1.h(\'9-5\'))},3:a(){B 2=\'\';0.1.m(\'4\',0.3.4);2+=0.3.6!=\'\'?\'6:\'+0.3.6+\';\':\'\';2+=0.3.5!=\'\'?\'9-5:\'+E(0.3.5)+\'F;\':\'\';2!=\'\'?0.1.m(\'8\',2):\'\';2==\'\'?0.1.G(\'8\'):\'\'}});7.f.e(\'i\',0.H+\'I/b.J\');7.K.L(7.j.M(\'b\')+\'9-n/o/9-n.y.o\')}});',52,52,'this|element|istayl|data|class|size|color|CKEDITOR|style|font|function|fontawesome|span|setData|add|dialog|fa|getStyle|fontawesomeDialog|plugins|editor|init|setAttribute|awesome|css|button|Insert|name|hasClass|Font|Awesome|getAttribute|template|icons|min|requires|widget|var|widgets|allowedContent|parseInt|px|removeAttribute|path|dialogs|js|document|appendStyleSheet|getPath|FontAwesome|upcast|return'.split('|'),0,{}))
+CKEDITOR.plugins.add('fontawesome', {
+	requires: 'widget',
+	icons: 'fontawesome',
+	init: function (editor) {
+		editor.widgets.add('FontAwesome', {
+			button: 'Insert Font Awesome',
+			template: '<span class="" style=""></span>',
+			dialog: 'fontawesomeDialog',
+			allowedContent: 'span(!fa){style}',
+			upcast: function (element) {
+				return element.name == 'span' && element.hasClass('fa')
+			},
+			init: function () {
+				this.setData('class', this.element.getAttribute('class'));
+				this.setData('color', this.element.getStyle('color'));
+				this.setData('size', this.element.getStyle('font-size'))
+			},
+			data: function () {
+				var istayl = '';
+				this.element.setAttribute('class', this.data.class);
+				istayl += this.data.color != '' ? 'color:' + this.data.color + ';': '';
+				istayl += this.data.size != '' ? 'font-size:' + parseInt(this.data.size) + 'px;': '';
+				istayl != '' ? this.element.setAttribute('style', istayl) : '';
+				istayl == '' ? this.element.removeAttribute('style') : ''
+			}
+		});
+		CKEDITOR.dialog.add('fontawesomeDialog', this.path + 'dialogs/fontawesome.js');
+		CKEDITOR.document.appendStyleSheet(CKEDITOR.plugins.getPath('fontawesome') + 'font-awesome/css/font-awesome.min.css')
+	}
+});
